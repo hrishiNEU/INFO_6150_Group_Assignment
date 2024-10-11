8f159d9867a6046ee7c31a58a42f923b6792677e
Rajvi-C <chokshi.ra@northeastern.edu>
2024-10-11 17:14:38 -0400

    Added folder structure and HTMl code for login page

---
+++ js/login.js
@@ -0,0 +1,14 @@
+// $(document).ready(function () {
+//   $("#loginForm").on("submit", function (event) {
+//     event.preventDefault();
+
+//     var username = $("#username").val();
+//     var password = $("#password").val();
+
+//     if (username === "admin" && password === "password123") {
+//       window.location.href = "home.html"; // Redirect to home page
+//     } else {
+//       $("#errorMessage").removeClass("d-none"); // Show error message
+//     }
+//   });
+// });
